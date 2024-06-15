7cbe360f8da657e91cb9a12e8660fab880ede0cc
juliankoh6 <138428500+juliankoh6@users.noreply.github.com>
2024-06-15 22:14:49 +0800

    Update email_sender.py
    add functions encrypt() and verify_password()

--- src/email sender/email_sender.py
+++ src/email sender/email_sender.py
@@ -1,5 +1,6 @@
 import smtplib
 from email.message import EmailMessage
+import bcrypt
 
 # Sends an email to receiver from the applications email
 def send_email(receiver_email, subject, body, sender_email='kohjulian150@gmail.com', sender_password='lbon slns xpev edgb'):
@@ -19,3 +20,14 @@ def send_email(receiver_email, subject, body, sender_email='kohjulian150@gmail.c
         print(f"Email sent to {receiver_email}")
     except Exception as e:
         print(f"Error sending email: {e}")
+
+
+# Function to encode a password for security
+def encrypt(password):
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return hashed_password.decode('utf-8')
+
+
+# Function to decode the password
+def verify_password(password, hashed_password):
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
